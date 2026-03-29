0c65444b1854e1884f441b29b17cd61641ab8970
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2026-03-29 14:07:06 +0200

    test: setup vitest and add validation unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   base: '/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    exclude: ['**/node_modules/**', '**/tests/**'],
+  },
 });
